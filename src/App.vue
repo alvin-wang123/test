a9f6ce5569c4197dbf0b67b769cb32fecf0b3954
Alvin Wang <alvin.wang@keyxentic.com>
2023-11-07 15:35:14 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { gsap } from 'gsap'
+console.log(gsap)
+</script>
+
+<template>
+  <header></header>
+
+  <main>
+    <div class="title-group">
+      <h1>welcomewelcomewelcomewelcome</h1>
+      <h2>apple</h2>
+      <h3>banana</h3>
+    </div>
+  </main>
+</template>
+
+<style scoped lang="scss">
+main {
+  .title-group {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 30%;
+    left: 50%;
+    transform: translateX(-50%);
+    h1 {
+      margin-bottom: 100px;
+    }
+    h2 {
+      margin-bottom: 100px;
+    }
+    h3 {
+    }
+  }
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import '@/style/cssReset.css'
+import '@/style/base.css'
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
